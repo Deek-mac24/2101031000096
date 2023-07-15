40b1b8c9edd980c42abb96d89783b9582bd083eb
Deek-mac24 <125550746+Deek-mac24@users.noreply.github.com>
2023-07-15 20:09:54 +0530

    Add files via upload

---
+++ Day 19 (10-7-23)/Addition.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Additon
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Eneter two Numbers:");
+
+            
+            int Number1 = Convert.ToInt32(Console.ReadLine());
+
+            
+            int Number2 = Convert.ToInt32(Console.ReadLine());
+
+            int Result = Number1 + Number2;
+            Console.WriteLine($"The Sum is: {Result}");
+            Console.WriteLine($"The Sum is: {Number1 + Number2}");
+            Console.ReadKey();
+
+        }
+    }
+}
---
+++ Day 19 (10-7-23)/Properties.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Properties
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.BackgroundColor = ConsoleColor.Red;
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.Title = "Understanding Console Class";
+            Console.WriteLine("BackgroundColor: Blue");
+            Console.WriteLine("ForegroundColor: White");
+            Console.WriteLine("Title: Understanding Console Class");
+
+            
+            Console.Beep();
+
+            Console.ReadKey();
+
+        }
+    }
+}
